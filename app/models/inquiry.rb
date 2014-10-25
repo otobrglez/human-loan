85b44973b676e646ea131ac9bb19e52127454f4c
Oto Brglez <otobrglez@gmail.com>
2014-10-25 22:26:25 +0100

    Update

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,4 +1,6 @@
 class Inquiry < ActiveRecord::Base
+  BASIC_RATE = 5.2
+
   belongs_to :user
   validates :reason, presence: true, length: 20..200
 
@@ -17,5 +19,30 @@ class Inquiry < ActiveRecord::Base
 
   def generate_score
     self.score = rand(10)
+  end
+
+  def rates
+    @rates ||= rates_cp
+  end
+
+  def rates_cp
+    rates_out=[]
+    scores = (0..9).to_a
+    scores.reverse.each do |i|
+      a = (scores.size-i-1)
+      rates_out[i]= BASIC_RATE + (a.to_i*0.5)
+    end
+
+    factors={
+      m_1: 1.to_f/12.to_f,
+      m_6: 1.to_f/2.to_f,
+      y_1: 1.to_f/1.to_f
+    }
+
+    {
+      m_1: Money.new((amount_cents.to_f * rates_out[score] * factors[:m_1]) + amount_cents, self.amount_currency),
+      m_6: Money.new((amount_cents.to_f * rates_out[score] * factors[:m_6]) + amount_cents, self.amount_currency),
+      y_1: Money.new((amount_cents.to_f * rates_out[score] * factors[:y_1]) + amount_cents, self.amount_currency)
+    }
   end
 end
--- spec/models/inquiry_spec.rb
+++ spec/models/inquiry_spec.rb
@@ -2,5 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Inquiry do
   it { should belong_to(:user) }
-  it { should respond_to :reason }
+
+  it "should do calculations" do
+    puts Inquiry.new(score: 9, amount_cents: 10000, amount_currency: "USD").rates.inspect
+  end
 end
+
+=begin
+
+9 -> 0 (best credit score => basic interest rate of 5,2%)
+8 -> 1 (basic interest rate of 5,2% plus one time 0,5% = 5,7)
+7 -> 2 (5,2% + 2*0,5 = 6,2%)
+6 -> 3
+5 -> 4
+4 -> 5 (5,2% 5*0,5=7,7%)
+3 -> 6
+2 -> 7
+1 -> 8
+0 -> 9 (5,2 +9*0,5= 9,7%)
+
+Duration
+1 month => 1/12
+6 months => 1/2
+12 months => 1/1
+
+=> (AMOUNT * interest rate * Duration) + amount
+
+Half a year with highest interest rate: (100 * 9,7% * 0,5) + 100 = 104,85
+Full year cheapest middle interest rate = 1000 * 7,7% * 1 + 1000 = 1077
+
+
+=end

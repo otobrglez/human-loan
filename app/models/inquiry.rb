e8ee55a52925b15f36f7d268e9452a460b6309b8
Oto Brglez <otobrglez@gmail.com>
2014-10-26 01:24:20 +0000

    Update

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -4,7 +4,7 @@ class Inquiry < ActiveRecord::Base
   validates :reason, presence: true, length: 20..200
 
   monetize :amount_cents
-  paginates_per 7
+  paginates_per 20
 
   scope :active, -> {
     joins(:user).order(created_at: :desc, expires_at: :asc)
